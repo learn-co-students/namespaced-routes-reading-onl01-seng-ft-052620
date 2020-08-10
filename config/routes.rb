0b447c13b0caa396c1cd036a05540a743fc22cab
shazy89 <erdo.shazy123@gmail.com>
2020-08-10 16:09:51 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   resources :authors, only: %i[show index] do
     resources :posts, only: %i[show index new edit]
   end
+# scope '/admin', module: 'admin' do
+#   resources :stats, only: [:index]
+# end
+
+namespace :admin do
+  resources :stats, only: [:index]
+end
 
   resources :posts, only: %i[index show new create edit update]
 
